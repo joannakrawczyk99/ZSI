51a18fec6ea1a1aac01e6183c2b9f0d63dd5211d
Joanna <joannakrawczyk99@gmail.com>
2022-04-10 09:13:20 +0200

    init

--- population.py
+++
@@ -1,8 +0,0 @@
-# creates starting population
-from main import population_size, population
-from route import create_route
-
-
-def create_population():
-    for i in range(population_size):
-        population.append(create_route())
